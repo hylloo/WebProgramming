0f253bb935a8734cb2e641086fb4ad510f226e2e
Iiro Pitkänen <106667418+hylloo@users.noreply.github.com>
2023-09-15 21:12:40 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,89 +11,98 @@ function enmptyTableContent(){
 
 }
 function initializeCode() {
-  const nameInput = document.getElementById("input-username");
-  const emailInput = document.getElementById("input-email");
-  const adminStatusInput = document.getElementById("input-admin");
-  const imageFileInput = document.getElementById("input-image");
+  const form = document.getElementById("user-form");
   const table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
-  const addUserButton = document.getElementById("submit-data");
   const clearTableButton = document.getElementById("clearTableButton");
 
-  addInitialUser("User 1", "user1@example.com", false);
-  addInitialUser("User 2", "user2@example.com", true);
+  addExampleUser("User 1", "user1@example.com", false);
+  addExampleUser("User 2", "user2@example.com", true);
 
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  addUserButton.addEventListener("click", function() {
-      const name = nameInput.value;
-      const email = emailInput.value;
-      const adminStatus = adminStatusInput.checked;
-      const imageFile = imageFileInput.files[0];
+    const nameInput = document.getElementById("input-username");
+    const emailInput = document.getElementById("input-email");
+    const adminStatusInput = document.getElementById("input-admin");
+    const imageFileInput = document.getElementById("input-image");
 
-      if (name.trim() === "" || email.trim() === "") {
-          alert("Please enter name and email");
-          return;
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const adminStatus = adminStatusInput.checked;
+    const imageFile = imageFileInput.files[0];
+
+    if (name.trim() === "" || email.trim() === "") {
+      alert("Please enter name and email");
+      return;
+    }
+
+    let existingRow = null;
+
+    for (const row of table.rows) {
+      if (row.cells[0].textContent === name) {
+        existingRow = row;
+        break;
       }
+    }
 
-      let existingRow = null;
-      for ( const row of table.rows){
-        if (row.cells[0].textContent === name){
-          existingRow = row;
-          break;
-        }
-      }
+    if (existingRow) {
+      existingRow.cells[1].textContent = email;
+      existingRow.cells[2].textContent = adminStatus ? "X" : "-";
+      displayImage(existingRow.cells[3], imageFile);
+    } 
+    else {
+      const newRow = table.insertRow(table.rows.length);
+      const cell1 = newRow.insertCell(0);
+      const cell2 = newRow.insertCell(1);
+      const cell3 = newRow.insertCell(2);
+      const cell4 = newRow.insertCell(3);
+      displayImage(cell4, imageFile);
 
-      if (existingRow){
-        existingRow.cells[1].textContent = email;
-        existingRow.cells[2].textContent = adminStatus ? "X": "-";
-        displayImage(existingRow.cells[3], imageFile);
-      }
-      else{
-        const newRow = table.insertRow(table.rows.length);
-        const cell1 = newRow.insertCell(0);
-        const cell2 = newRow.insertCell(1);
-        const cell3 = newRow.insertCell(2);
-        const cell4 = newRow.insertCell(3);
-        displayImage(cell4, imageFile);
+      cell1.innerHTML = name;
+      cell2.innerHTML = email;
+      cell3.innerHTML = adminStatus ? "X" : "-";
+      cell4.innerHTML = `<img src="${imageUrl}" alt="User Image" style="max-width: 64px; max-height: 64px;">`;
 
-        cell1.innerHTML = name;
-        cell2.innerHTML = email;
-        cell3.innerHTML = adminStatus ? "X" : "-";
-        cell4.innerHTML = `<img src="${imageUrl}" alt="User Image" style="max-width: 64px; max-height: 64px;">`;
-
-        nameInput.value = "";
-        emailInput.value = "";
-        adminStatusInput.checked = false;
-        imageInput.value = "";
-      }
-      
+      nameInput.value = "";
+      emailInput.value = "";
+      adminStatusInput.checked = false;
+      imageFileInput.value = "";
+    }
   });
-  clearTableButton.addEventListener("click", function() {
+
+  clearTableButton.addEventListener("click", function () {
     while (table.firstChild) {
       table.removeChild(table.firstChild);
+    }
+  });
+
+  function addExampleUser(name, email, adminStatus) {
+    const newRow = table.insertRow(table.rows.length);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = name;
+    cell2.innerHTML = email;
+    cell3.innerHTML = adminStatus ? "X" : "-";
   }
-});
 
-function addInitialUser(name, email, adminStatus, imageFileName) {
-  const existingRow = table.insertRow(table.rows.length);
-  const cell1 = existingRow.insertCell(0);
-  const cell2 = existingRow.insertCell(1);
-  const cell3 = existingRow.insertCell(2);
+  function displayImage(cell, imageFile) {
+    if (imageFile) {
+      const image = document.createElement("img");
+      image.style.maxWidth = "64px";
+      image.style.maxHeight = "64px";
+      image.src = URL.createObjectURL(imageFile);
 
-  cell1.innerHTML = name;
-  cell2.innerHTML = email;
-  cell3.innerHTML = adminStatus ? "X" : "-";
+      cell.innerHTML = "";
+      cell.appendChild(image);
+    } else {
+      cell.innerHTML = "";
+    }
+  }
 }
 
 
-function displayImage(cell, imageFile) {
-  const image = document.createElement("img");
-  image.style.maxWidth = "64px";
-  image.style.maxHeight = "64px";
-  image.src = URL.createObjectURL(imageFile);
 
-  cell.innerHTML = "";
-  cell.appendChild(image);
-}
 
-}
 
